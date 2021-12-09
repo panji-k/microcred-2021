4b6ceaebfaf7649d58ab9c7ba0ba67a3016dd9ce
Panji Kusmanto <panji.k@outlook.co.id>
2021-12-09 23:54:34 +0700

    Plot Menggunakan Streamlit

--- project.py
+++ project.py
@@ -14,13 +14,18 @@ st.title('Analisa Survey Perkembangan Data Scientist di Indonesia')
 
 # persiapan data frame global
 st.header('Sample Data Survey Global')
-df1 = pd.read_csv('https://filedn.com/lePVfyAoiNxFBjMKNqcr2O7/MICROCREDENTIAL/ProjectTugasAkhir/survey_2021_responses.csv')
-df_sample_global = df1.head()
+df_global = pd.read_csv('https://filedn.com/lePVfyAoiNxFBjMKNqcr2O7/MICROCREDENTIAL/ProjectTugasAkhir/survey_2021_responses.csv')
+df_sample_global = df_global.head()
 st.write(df_sample_global)
 
 # persiapan data frame Indonesia
 st.header('Sample Data Survey Indonesia')
-df_indonesia=df1[df1["Q3"]=="Indonesia"]
+df_indonesia=df_global[df_global["Q3"]=="Indonesia"]
 st.write('Dataset sudah difilter hanya Indonesia:')
 df_sample_indo = df_indonesia.head()
 st.write(df_sample_indo)
+
+st.header('Kategori Umur Responden Indonesia')
+st.write(df_indonesia['Q1'].value_counts().sort_index().plot(kind='bar'))
+st.header('Kategori Umur Responden Global')
+st.write(df_global['Q1'].value_counts().sort_index().plot(kind='bar'))
