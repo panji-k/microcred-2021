bf003af06acb380c84dbcc6662fa99a2959a116c
Panji Kusmanto <panji.k@outlook.co.id>
2021-12-09 23:42:21 +0700

    Prepare & Load Data Frame

---
+++ project.py
@@ -0,0 +1,26 @@
+# import modul-modul yang dibutuhkan
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# me-non aktifkan peringatan pada python
+import warnings 
+warnings.filterwarnings('ignore')
+
+# judul project
+st.title('Analisa Survey Perkembangan Data Scientist di Indonesia')
+
+# persiapan data frame global
+st.header('Sample Data Survey Global')
+df1 = pd.read_csv('kaggle_survey_2021_responses.csv')
+df_sample_global = df1.head()
+st.write(df_sample_global)
+
+# persiapan data frame Indonesia
+st.header('Sample Data Survey Indonesia')
+df_indonesia=df1[df1["Q3"]=="Indonesia"]
+st.write('Dataset sudah difilter hanya Indonesia:')
+df_sample_indo = df_indonesia.head()
+st.write(df_sample_indo)
