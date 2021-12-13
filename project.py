b2af8da013cd4a9da42c42e432b55d85478286aa
Panji K <panji.k@outlook.co.id>
2021-12-13 17:35:30 +0700

    Perbandingan Bahasa Pemrograman yang Digunakan

--- project.py
+++ project.py
@@ -85,6 +85,49 @@ barPlot()
 
 st.header('Perbandingan jumlah responden Global berdasarkan pengalaman programming')
 def barPlot():
-    st.bar_chart(df_global_full['Q6'].value_counts().reindex(['Belum Pernah', '< 1 years', '1-3 years', '3-5 years','5-10 years','10-20 years','20+ years']))
+    st.bar_chart(df_global['Q6'].value_counts().reindex(['Belum Pernah', '< 1 years', '1-3 years', '3-5 years','5-10 years','10-20 years','20+ years']))
 barPlot()
 
+# Perbandingan Bahasa Pemrograman yang Digunakan
+st.header('Perbandingan Bahasa Pemrograman yang Digunakan di Indonesia')
+def barPlot():
+    languages = ['Python','R','SQL','C','C++','Java',"Javascript","Julia","Bash","Matlab"]
+    python=df_indonesia.Q7_Part_1.value_counts()[0]
+    r= df_indonesia.Q7_Part_2.value_counts()[0]
+    sql=df_indonesia.Q7_Part_3.value_counts()[0]
+    c=df_indonesia.Q7_Part_4.value_counts()[0]
+    c_plus =df_indonesia.Q7_Part_5.value_counts()[0]
+    java = df_indonesia.Q7_Part_6.value_counts()[0]
+    javascript=df_indonesia.Q7_Part_7.value_counts()[0]
+    julia = df_indonesia.Q7_Part_8.value_counts()[0]
+    bash = df_indonesia.Q7_Part_10.value_counts()[0]
+    matlab = df_indonesia.Q7_Part_11.value_counts()[0]
+    values_1 = [python,r,sql,c,c_plus,java,javascript,julia,bash,matlab]
+    plt.bar(languages,values_1)
+    plt.xticks(rotation=45)
+    plt.title("Bahasa Pemrograman yang Paling Banyak Digunakan di Indonesia")
+barPlot()
+
+st.header('Perbandingan Bahasa Pemrograman yang Digunakan secara Global')
+def barPlot():
+    languages_global = ['Python','R','SQL','C','C++','Java',"Javascript","Julia","Swift","Bash","Matlab"]
+    python=df_global.Q7_Part_1.value_counts()[0]
+    r= df_global.Q7_Part_2.value_counts()[0]
+    sql=df_global.Q7_Part_3.value_counts()[0]
+    c=df_global.Q7_Part_4.value_counts()[0]
+    c_plus =df_global.Q7_Part_5.value_counts()[0]
+    java = df_global.Q7_Part_6.value_counts()[0]
+    javascript=df_global.Q7_Part_7.value_counts()[0]
+    julia = df_global.Q7_Part_8.value_counts()[0]
+    swift = df_global.Q7_Part_9.value_counts()[0]
+    bash = df_global.Q7_Part_10.value_counts()[0]
+    matlab = df_global.Q7_Part_11.value_counts()[0]
+    values_2 = [python,r,sql,c,c_plus,java,javascript,julia,swift,bash,matlab]
+    plt.bar(languages_global,values_2)
+    plt.xticks(rotation=45)
+    plt.title("Bahasa Pemrograman yang Paling Banyak Digunakan di Dunia")
+barPlot()
+
+st.write('Bahasa pemrograman yang paling populer adalah Python, seperti yang diharapkan baik di Indonesia maupun secara global.')
+st.write('')
+st.write('Setelah kita melakukan beberapa penelitian sederhana, Python banyak digunakan dari bahasa lain karena: Python lebih produktif, memiliki daya saing dan inovasi yang cepat, memiliki berbagai library, framework, dan komunitas yang sangat besar.')'
